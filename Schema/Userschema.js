f97984e83091f09ada0a8bcadc143a3cd0afcf57
M.Dinesh <dinesh372003@gmail.com>
2021-08-16 12:49:40 +0530

    Connected Mongodb with Userschema

---
+++ Schema/Userschema.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const usersSchema=new Schema(
+{
+    email:
+    {
+        type:String,
+        required:true,
+    },
+    name:
+    {
+        type:String,
+        required:true,
+    },
+    password:
+    {
+        type:String,
+        required:true
+    },
+    type:
+    {
+        type:String,
+        required:true,
+    }
+},{timestamps:true});
+const user=mongoose.model("User",usersSchema);
+module.exports=user;
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Be Happy");
+const express=require("express");
+const app=express();
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+app.set("view engine","ejs");
+const database="mongodb+srv://dinesh372003:Dinesh@741@school.0eq55.mongodb.net/Project?retryWrites=true&w=majority";
+const session=require("express-session");
+const User=require("./Schema/Userschema");
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(database,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(result=>
+    {
+        app.listen(3000);
+        console.log("Listening on Port 3000");
+    })
+    .catch((err)=>console.log(err))
+
