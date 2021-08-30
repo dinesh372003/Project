dd781916218d8ef8165c6677c72af42ee1574f8b
M.Dinesh <dinesh372003@gmail.com>
2021-08-31 00:10:25 +0530

    Making redirecting to class page

--- Controlller/Dashboardcontroller.js
+++ Controlller/Dashboardcontroller.js
@@ -1,7 +1,19 @@
 const Class=require("../Schema/Classschema");
 const Users=require("../Schema/Userschema");
 const mongoose=require("mongoose");
+var classe;
 
+
+function storeinclass(result)
+{
+    const variable={};
+    variable["subteacher"]=result.subteacher;
+    variable["subjects"]=result.subjects;
+    variable["students"]=result.students;
+    variable["mainteacher"]=result.mainteacher;
+    variable["id"]=result.id;
+    return variable;
+}
 const dashboard=(req,res)=>
 {
     res.render("dashboard",{User:req.user,title:"Dashboard"});
