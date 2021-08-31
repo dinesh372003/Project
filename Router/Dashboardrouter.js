caf30be812bf1f9ff635d678658394553f010533
M.Dinesh <dinesh372003@gmail.com>
2021-08-31 14:09:56 +0530

    class page

--- Router/Dashboardrouter.js
+++ Router/Dashboardrouter.js
@@ -1,14 +1,36 @@
 const express=require("express");
 const router=express.Router();
 const dashcont=require("../Controlller/Dashboardcontroller");
-router.get("/",dashcont.dashboard);
-router.get("/newclass",(req,res)=>
-{
-    res.render("newclass",{User:req.user,title:"New Class"});
-});
-router.get("/joinclass",(req,res)=>
-{
-    res.render("joinclass",{User:req.user,title:"Join Class"});
-});
-router.post("/newclass",dashcont.newclass);
+const Class = require("../Schema/Classschema");
+//Get request
+
+    //Rendering dashboard
+    router.get("/",dashcont.dashboard);
+
+    //Rendering new class page
+    router.get("/newclass",(req,res)=>
+    {
+        res.render("newclass",{User:req.user,title:"New Class"});
+    });
+
+    //Rendering join class page
+    router.get("/joinclass",(req,res)=>
+    {
+        res.render("joinclass",{User:req.user,title:"Join Class"});
+    });
+
+    //Rendering class page
+    router.get("/:id",(req,res)=>
+    {
+        const id=req.params.id;
+        Class.findById(id)
+        .then(result=>
+            res.render("class",{User:req.user,title:result.classname,Class:result})
+        )
+        .catch(err=>console.log(err));
+    })
+
+//Post Request
+    //Getting new class post request
+    router.post("/newclass",dashcont.newclass);
 module.exports=router;
